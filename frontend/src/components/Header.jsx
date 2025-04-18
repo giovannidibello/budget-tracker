2426b98fd90d2a027b56582c4cd8f4107b4e9fd8
Giovanni <dibellogio@gmail.com>
2025-04-18 19:10:58 +0200

    fix header color

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,15 @@
 // Header.jsx
 
 import { Container, Navbar } from "react-bootstrap";
-
-// importo il modulo per il link
-// import { Link } from "react-router-dom"
-
+import { Link } from 'react-router-dom';
 
 export default function Header() {
 
     return (
 
-        <Navbar bg="primary" variant="dark">
+        <Navbar style={{ backgroundColor: '#abc799' }} variant="dark">
             <Container>
-                <Navbar.Brand href="#">Budget Tracker</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" style={{ fontSize: '2rem', color: 'black' }}>Budget Tracker</Navbar.Brand>
             </Container>
         </Navbar>
 
